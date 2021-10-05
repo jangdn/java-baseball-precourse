1a6c171437ed84d7d3122c42dffd40e19e1b6580
jangwooan <jangdn@nate.com>
2021-10-06 02:20:54 +0900

    fix(lombok delete): lombok 의존성 삭제
    - lombok lib 없이도 충분히 어렵지 않게 개발할 수 있어 제외함.

--- src/main/java/baseball/GameError.java
+++ src/main/java/baseball/GameError.java
@@ -1,12 +1,14 @@
 package baseball;
 
-import lombok.Getter;
 
-@Getter
 public enum GameError {
-    INVALID_INPUT_STRING("Not Valid Input String");
+    INVALID_INPUT_STRING("[ERROR] 잘못된 input입니다.");
 
     private final String description;
+
+    public String getDescription(){
+        return this.description;
+    }
 
     GameError(String description) {
         this.description = description;
--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -1,14 +1,15 @@
 package baseball;
 
-import lombok.Getter;
-
-@Getter
 public enum GameResult {
     STRIKE("스트라이크"),
     BALL("볼"),
     MISS("미스");
 
     private final String description;
+
+    public String getDescription(){
+        return this.description;
+    }
 
     GameResult(String description) {
         this.description = description;
